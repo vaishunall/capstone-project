0312b8b151fc13f912f655083444bfdbb902d395
vaishunall <vaishunalla@yahoo.com>
2023-11-07 01:25:58 -0600

    Basic Logo

---
+++ Capstone/Capstone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Capstone
+//
+//  Created by Vaishu Nalla on 11/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
