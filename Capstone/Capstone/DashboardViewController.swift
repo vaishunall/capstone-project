e50ebc446ae3b0c89c9e1e3815d4769ba5f59d65
vaishunall <vaishunalla@yahoo.com>
2023-11-14 02:08:00 -0600

    week 2 updates

---
+++ Capstone/Capstone/DashboardViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DashboardViewController.swift
+//  Capstone
+//
+//  Created by Vaishu Nalla on 11/14/23.
+//
+
+import Foundation
+import UIKit
+
+class DashboardViewController: UIViewController {
+    
+
+    
+    override func viewDidLoad() {
+        
+        
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+}
--- Capstone/Capstone/ViewController.swift
+++ Capstone/Capstone/ViewController.swift
@@ -7,11 +7,53 @@
 
 import UIKit
 
+extension UIView {
+    func dropShadow(scale: Bool = true) {
+        layer.masksToBounds = false
+        layer.shadowColor = UIColor.black.cgColor;
+        layer.shadowOpacity = 0.2;
+        layer.shadowOffset = CGSize(width: 0, height: 5);
+        layer.shadowRadius = 1;
+        layer.shouldRasterize = true
+        layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+    }
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calendarTitleText: UILabel!
+    
+    @IBOutlet weak var CalendarView: UIStackView!
+    @IBOutlet weak var sunView: UIView!
+    @IBOutlet weak var monView: UIView!
+    @IBOutlet weak var tueView: UIView!
+    @IBOutlet weak var wedView: UIView!
+    @IBOutlet weak var thuView: UIView!
+    @IBOutlet weak var friView: UIView!
+    @IBOutlet weak var satView: UIView!
+    
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        sunView.dropShadow()
+        monView.dropShadow()
+        tueView.dropShadow()
+        wedView.dropShadow()
+        thuView.dropShadow()
+        friView.dropShadow()
+        satView.dropShadow()
+        
+        (sunView.layer.cornerRadius,
+         monView.layer.cornerRadius,
+        tueView.layer.cornerRadius,
+        wedView.layer.cornerRadius,
+        thuView.layer.cornerRadius,
+        friView.layer.cornerRadius,
+         satView.layer.cornerRadius) = (6, 6,  6, 6, 6, 6, 6);
+        
     }
 
 
